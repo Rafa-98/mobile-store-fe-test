a9b79dc756f091cb0aadcbff319270a17728913f
Rafa-98 <rmendeznieves98@gmail.com>
2023-07-14 16:18:46 -0400

    updated project structure

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const routes = [
+  {
+    path: "/",
+    element: <Products />,
+  },
+  {
+    path: "/details",
+    element: <ProductDetails />
+  }
+];
+
+const router = createBrowserRouter(routes);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
